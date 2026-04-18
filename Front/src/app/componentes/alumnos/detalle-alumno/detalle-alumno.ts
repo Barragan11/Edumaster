13234b708002848e5e08979c48f472807bec17aa
Luis Ángel Barragán González <158596646+Barragan11@users.noreply.github.com>
2026-04-17 21:33:56 -0600

    Delete Front/src/app/componentes/alumnos/detalle-alumno directory

--- Front/src/app/componentes/alumnos/detalle-alumno/detalle-alumno.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-detalle-alumno',
-  imports: [],
-  templateUrl: './detalle-alumno.html',
-  styleUrl: './detalle-alumno.css',
-})
-export class DetalleAlumno {}
